9b806da7478b60da49db1f8bd555172fe4e08a19
theonephoenixoffical <58648981+theonephoenixoffical@users.noreply.github.com>
2019-12-08 00:50:57 -0500

    Update has-auth.php

--- has-auth.php
+++ has-auth.php
@@ -5,5 +5,8 @@ include "run-auth.php";
 Account Name Here: <input type="checkbox" name="option1"> stock: <?php echo $opt1stk;?><br>
 Account Name Here: <input type="checkbox" name="option2"> stock: <?php echo $opt2stk;?><br>
 Auth Key: <input type="text" name="auth">
+  <hr>
+  <?php echo $account;?>
+  <hr>
 <button type="submit">
 </form>

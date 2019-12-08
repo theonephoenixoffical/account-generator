56e831e0e5a9ee72e315d49a59bd568ff666a375
theonephoenixoffical <58648981+theonephoenixoffical@users.noreply.github.com>
2019-12-08 00:37:44 -0500

    Create no-auth.php

---
+++ no-auth.php
@@ -0,0 +1,7 @@
+<?php
+include "run.php";
+?>
+<form action="no-auth.php" method="post">
+Account Name Here: <input type="checkbox" name="option1"> stock: <?php echo $opt1stk;?><br>
+Account Name Here: <input type="checkbox" name="option2"> stock: <?php echo $opt2stk;?><br>
+</form>

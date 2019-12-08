063668667de5b4ef579734913d2d4f1c16c30d65
theonephoenixoffical <58648981+theonephoenixoffical@users.noreply.github.com>
2019-12-08 00:40:25 -0500

    Update and rename auth.php to has-auth.php

---
+++ has-auth.php
@@ -0,0 +1,9 @@
+<?php
+include "run-auth.php";
+?>
+<form action="no-auth.php" method="post">
+Account Name Here: <input type="checkbox" name="option1"> stock: <?php echo $opt1stk;?><br>
+Account Name Here: <input type="checkbox" name="option2"> stock: <?php echo $opt2stk;?><br>
+Auth Key: <input type="text" name="auth">
+<button type="submit">
+</form>

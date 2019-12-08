527d4789f7c845a48696797d58dcf328d7b27857
theonephoenixoffical <58648981+theonephoenixoffical@users.noreply.github.com>
2019-12-08 00:52:05 -0500

    Update no-auth.php

--- no-auth.php
+++ no-auth.php
@@ -4,4 +4,8 @@ include "run.php";
 <form action="no-auth.php" method="post">
 Account Name Here: <input type="checkbox" name="option1"> stock: <?php echo $opt1stk;?><br>
 Account Name Here: <input type="checkbox" name="option2"> stock: <?php echo $opt2stk;?><br>
+  <hr>
+  <?php echo $account;?>
+  <hr>
+  <button type="submit">Submit</button>
 </form>
